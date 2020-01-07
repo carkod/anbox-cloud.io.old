42fefa34d718b12d730c51710af00e01bbaad625
Carlos Wu <carlos.fei@canonical.com>
2020-01-07 14:21:10 +0000

    save latest changes

--- webapp/api/response.py
+++ webapp/api/response.py
@@ -1,27 +1,17 @@
 import os
-from urllib.parse import quote
 
-import flask
-from django_openid_auth.teams import TeamsRequest, TeamsResponse
-from flask_openid import OpenID
 from webapp import authentication
-from webapp.api.exceptions import ApiCircuitBreaker, ApiError, ApiResponseError
-from webapp.extensions import csrf
-from webapp.login.macaroon import MacaroonRequest, MacaroonResponse
-
-login = flask.Blueprint(
-    "login", __name__, 
-    template_folder="/templates", 
-    static_folder="/static"
+from webapp import api
+from webapp.api.exceptions import (
+    AgreementNotSigned,
+    ApiResponseDecodeError,
+    ApiResponseError,
+    ApiResponseErrorList,
+    MacaroonRefreshRequired,
+    MissingUsername,
 )
 
-LOGIN_URL = os.getenv("LOGIN_URL", "https://login.ubuntu.com")
-
-LP_CANONICAL_TEAM = "canonical"
-
-open_id = OpenID(
-    stateless=True, safe_roots=[], extension_responses=[MacaroonResponse, TeamsResponse]
-)
+api_session = api.requests.Session(timeout=(1, 6))
 
 def process_response(response):
     try:
@@ -285,90 +275,3 @@ def post_close_channel(session, snap_id, json):
         raise MacaroonRefreshRequired
 
     return process_response(response)
-
-
-
-def login_blueprint(app, testing=False):
-
-    @login.route("/login", methods=["GET", "POST"])
-    
-    @csrf.exempt
-    @open_id.loginhandler
-    def login_handler():
-        if authentication.is_authenticated(flask.session):
-            return flask.redirect(open_id.get_next_url())
-
-        try:
-            root = authentication.request_macaroon()
-        except ApiResponseError as api_response_error:
-            if api_response_error.status_code == 401:
-                return login.redirect(login.url_for(".logout"))
-            else:
-                return login.abort(502, str(api_response_error))
-        except ApiCircuitBreaker:
-            login.abort(503)
-        except ApiError as api_error:
-            return login.abort(502, str(api_error))
-
-        openid_macaroon = MacaroonRequest(caveat_id=authentication.get_caveat_id(root))
-        login.session["macaroon_root"] = root
-.
-        lp_teams = TeamsRequest(query_membership=[LP_CANONICAL_TEAM])
-
-        return open_id.try_login(
-            LOGIN_URL,
-            ask_for=["email", "nickname", "image"],
-            ask_for_optional=["fullname"],
-            extensions=[openid_macaroon, lp_teams],
-        )
-
-
-    @open_id.after_login
-    def after_login(resp):
-        login.session["macaroon_discharge"] = resp.extensions["macaroon"].discharge
-        if not resp.nickname:
-            return login.redirect(LOGIN_URL)
-
-        try:
-            account = dashboard.get_account(login.session)
-            login.session["openid"] = {
-                "identity_url": resp.identity_url,
-                "nickname": account["username"],
-                "fullname": account["displayname"],
-                "image": resp.image,
-                "email": account["email"],
-                "is_canonical": LP_CANONICAL_TEAM in resp.extensions["lp"].is_member,
-            }
-            owned, shared = logic.get_snap_names_by_ownership(account)
-            login.session["user_shared_snaps"] = shared
-
-        except ApiCircuitBreaker:
-            login.abort(503)
-        except Exception:
-            login.session["openid"] = {
-                "identity_url": resp.identity_url,
-                "nickname": resp.nickname,
-                "fullname": resp.fullname,
-                "image": resp.image,
-                "email": resp.email,
-            }
-
-        return login.redirect(open_id.get_next_url())
-
-
-    @login.route("/logout")
-    def logout():
-        no_redirect = login.request.args.get("no_redirect", default="false")
-
-        if authentication.is_authenticated(login.session):
-            authentication.empty_session(login.session)
-
-        if no_redirect == "true":
-            return login.redirect("/")
-        else:
-            redirect_url = quote(login.request.url_root, safe="")
-            return login.redirect(
-                f"{LOGIN_URL}/+logout?return_to={redirect_url}&return_now=True"
-            )
-
-    return login
--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,6 @@
 from canonicalwebteam.flask_base.app import FlaskBase
 from flask import render_template
+from webapp.login.views import login_blueprint
 
 # Rename your project below
 app = FlaskBase(
@@ -10,8 +11,10 @@ app = FlaskBase(
     template_404="404.html",
     template_500="500.html",
 )
-
+app.register_blueprint(login_blueprint(app))
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
